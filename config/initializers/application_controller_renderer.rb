ed1818e3f3d1d6689a2e7cf12e7c6ae9b9452224
藤本僚太 <fujiraidar@fujimotoryoufutoshinoMacBook-puro.local>
2019-11-13 13:48:20 +0900

    Rails1-10

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'd546433c63da72e00ebfa6925e4c8fc26c08cfbe7f8d62acba2511002ee1f7645b1da418aaa74f270db738c46332000599d81214cc9c5ab2f59956e4757dedc7'
